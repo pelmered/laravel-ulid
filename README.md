ff7a74dc4329c3ff6c1f46a41d0877e53d8fd3e1
Peter Elmered <peter@elmered.com>
2024-11-22 14:27:16 +0100

    Set up environment for testing both SQLite and MySQL

--- README.md
+++ README.md
@@ -15,3 +15,23 @@ This package improves the ULID support in Laravel with the following features:
 - [ ] Add changelog
 - [ ] Add license
 - [ ] Add badges
+
+
+
+
+## Running tests
+
+Run the test with `vendor/bin/pest` or `composer test`.
+
+The test suite has some tests that require a MySQL database.
+To run these tests, you need to create a `.env` file in the root of the project with the following contents,
+adjusted for your setup:
+```
+DB_HOST=127.0.0.1
+DB_PORT=3306
+DB_DATABASE=laravel_ulid_testing
+DB_TESTING_DATABASE=laravel_ulid_testing
+DB_USERNAME=root
+DB_PASSWORD=
+```
+You can also skip the MySQL tests the tests by running `vendor/bin/pest --exclude-group=mysql`.
--- tests/TestbenchTestCase.php
+++ tests/TestbenchTestCase.php
@@ -11,6 +11,7 @@ use PhpStaticAnalysis\Attributes\Returns;
 use PhpStaticAnalysis\Attributes\Type;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Bootstrap\LoadEnvironmentVariables;
 
 #[WithMigration]
 class TestbenchTestCase extends \Orchestra\Testbench\TestCase
@@ -19,11 +20,38 @@ class TestbenchTestCase extends \Orchestra\Testbench\TestCase
     //use DatabaseTransactions;
     use RefreshDatabase;
 
+    protected function usesMySqlConnection($app): void
+    {
+        tap($app['config'], static function (Repository $config) {
+            $config->set('database.default', 'mysql');
+            $config->set('database.connections.mysql', [
+                'driver'   => 'mysql',
+                'host'     => config('database.connections.mysql.host', '127.0.0.1'),
+                'port'     => config('database.connections.mysql.port', '3306'),
+                'database' => config('database.connections.mysql.database', 'laravel_ulid_testing'),
+                'username' => config('database.connections.mysql.username', 'root'),
+                'password' => config('database.connections.mysql.password', null),
+                'prefix'   => '',
+            ]);
+
+        });
+        //$app['config']->set('database.default', 'mysql');
+    }
+
+    protected function usesSqliteConnection($app): void
+    {
+        $app['config']->set('database.default', 'sqlite');
+    }
 
     #[Param(app: Application::class)] // the second array
     #[Returns('void')]
     protected function defineEnvironment($app)
     {
+        // make sure, our .env file is loaded
+        $app->useEnvironmentPath(__DIR__.'/..');
+        $app->bootstrapWith([LoadEnvironmentVariables::class]);
+        parent::getEnvironmentSetUp($app);
+
         // Setup default database to use sqlite :memory:
         tap($app['config'], static function (Repository $config) {
             $config->set('database.default', 'testbench');
